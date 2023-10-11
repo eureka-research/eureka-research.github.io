ef95b0bc1d9e95966e1f92f0a398aeb3f32c5517
Will Liang <willjhliang@gmail.com>
2023-10-11 14:32:07 -0400

    updated upside down spin

--- videos/regen_video.sh
+++ videos/regen_video.sh
@@ -1,7 +1,7 @@
 # To fix the weird duration metadata error for videos generated with IsaacSim
 set -x
 
-input_folder=task_final
+input_folder=.
 # Iterate over each MP4 file in the input folder
 for file in "$input_folder"/*.mp4; do
     # Extract the filename without extension
